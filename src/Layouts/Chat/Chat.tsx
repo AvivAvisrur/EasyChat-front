e8cf5b612eae7986a6ae2ac5b45703b24275757a
Aviv Avisrur <aviv4588@gmail.com>
2022-12-28 21:17:59 +0200

    finished the main layout shell  style

--- src/Layouts/Chat/Chat.tsx
+++ src/Layouts/Chat/Chat.tsx
@@ -1,7 +1,15 @@
+import FooterBar from '../../Components/MainBar/FooterBar/FooterBar'
+import MainChat from '../../Components/MainBar/MainChat/MainChat'
+import { MainPanel } from '../../Components/MainBar/MainPanel/MainPanel'
+
 import './Chat.css'
 
 export function Chat() {
-    return (
-        <div className="chat-window">Chat</div>
-    )
+  return (
+    <main className='main-container'>
+      <MainPanel />
+      <MainChat />
+      <FooterBar />
+    </main>
+  )
 }
--- src/Layouts/Panel/Panel.tsx
+++ src/Layouts/Panel/Panel.tsx
@@ -1,16 +1,17 @@
-import "./Panel.css"
+import './Panel.css'
 
 type Props = {
   children: Array<JSX.Element> | JSX.Element
-  justifyContent?: "flex-start" | "flex-end" | "center" | "space-between" | "space-around" | "space-evenly"  
-};
+  justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly'
+  panelHeight?: number
+}
 
-export function Panel(props:Props) {
-    const styles = { justifyContent: props.justifyContent ?? "flex-start" }
+export function Panel(props: Props) {
+  const styles = { justifyContent: props.justifyContent ?? 'flex-start', height: props.panelHeight && `${props.panelHeight}px` }
 
-    return (
-      <div className="panel-box" style={styles}>
-        {props.children}
-      </div>
-    );
+  return (
+    <div className='panel-box' style={styles}>
+      {props.children}
+    </div>
+  )
 }
